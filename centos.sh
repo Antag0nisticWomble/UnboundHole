fe18345f2bc495b69ebeda5b2294bedc48dcee7b
Antag0nisticWomble <53240891+Antag0nisticWomble@users.noreply.github.com>
2022-09-04 15:00:24 +0000

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -99,6 +99,7 @@ update_crontab
 
 echo -e "$INFO starting and enabling unbound service $END"
 echo -e " "
+chown -R unbound:unbound /var/lib/unbound
 sudo systemctl enable unbound
 sudo systemctl restart unbound
 
